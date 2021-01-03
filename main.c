4834d121ce837b834dd3f94f3f97181eb8f7758a
asraa-anazi <asraa.anazi@sci.ku.edu.kw>
2021-01-03 17:14:22 +0300

    main.c 1 asraa

---
+++ main.c
@@ -0,0 +1,24 @@
+//Name:Asraa & Nour
+//Project part 1
+
+#include <stdio.h>
+
+
+
+
+
+   int main(){
+      int x;
+     
+        while(1){
+           scanf("%d",&x);
+
+
+       }
+
+
+
+
+
+  return 0 ;
+ }
